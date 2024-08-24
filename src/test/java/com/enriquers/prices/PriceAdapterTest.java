517565ff62267914bcad28218727056d2496837f
EnriqueRS <enrique.rios.santos@gmail.com>
2024-08-24 12:34:00 +0200

    Mockito added

--- src/test/java/com/enriquers/prices/PriceAdapterTest.java
+++ src/test/java/com/enriquers/prices/PriceAdapterTest.java
@@ -41,167 +41,312 @@ import static com.enriquers.prices.PriceTestsHelper.START_DATE_3;
 import static com.enriquers.prices.PriceTestsHelper.START_DATE_4;
 import static com.enriquers.prices.PriceTestsHelper.START_DATE_5;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
+import com.enriquers.prices.domain.price.model.Price;
+import com.enriquers.prices.domain.price.model.PriceQuery;
 import com.enriquers.prices.infrastructure.price.persistence.PriceRepository;
+import com.enriquers.prices.infrastructure.price.persistence.adapter.PriceAdapter;
 import com.enriquers.prices.infrastructure.price.persistence.model.PriceEntity;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 
-@ExtendWith(SpringExtension.class)
-@DataJpaTest
-@AutoConfigureTestDatabase(replace = Replace.NONE)
+@ExtendWith(MockitoExtension.class)
+//@DataJpaTest
+//@AutoConfigureTestDatabase(replace = Replace.NONE)
 class PriceAdapterTest {
 
-  @Autowired
+  @Mock
   private PriceRepository priceRepository;
 
+  @InjectMocks
+  private PriceAdapter priceAdapter;
 
   @Test
+  @DisplayName("Given existing price, when findPrice is called, then returns expected price (Test 1)")
   void givenExistingPrice_whenFindPrice_thenReturnsPriceTest1() {
     LocalDateTime requestTime = REQUEST_TIME_1;
     Integer productId = PRODUCT_ID_1;
     Integer brandId = BRAND_ID_1;
 
-    Optional<PriceEntity> priceEntityOptional = priceRepository.findPVP(brandId, productId, requestTime);
+    Mockito.when(priceRepository.findPVP(brandId, productId, requestTime))
+        .thenReturn(Optional.of(PriceEntity.builder()
+            .brandId(BRAND_ID_1)
+            .productId(PRODUCT_ID_1)
+            .priceList(PRICE_LIST_1)
+            .curr(CURRENCY_VALUE_1)
+            .finalPrice(FINAL_PRICE_1)
+            .startDate(START_DATE_1)
+            .endDate(END_DATE_1)
+            .build()));
 
-    assertNotNull(priceEntityOptional);
-    assertFalse(priceEntityOptional.isEmpty());
-    PriceEntity price = priceEntityOptional.get();
+    Price priceExpected = Price.builder()
+        .brandId(BRAND_ID_1)
+        .productId(PRODUCT_ID_1)
+        .priceList(PRICE_LIST_1)
+        .curr(CURRENCY_VALUE_1)
+        .finalPrice(FINAL_PRICE_1)
+        .startDate(START_DATE_1)
+        .endDate(END_DATE_1)
+        .build();
 
-    assertEquals(FINAL_PRICE_1, price.getFinalPrice());
-    assertEquals(BRAND_ID_1, price.getBrandId());
-    assertEquals(PRODUCT_ID_1, price.getProductId());
-    assertEquals(PRICE_LIST_1, price.getPriceList());
-    assertEquals(CURRENCY_VALUE_1, price.getCurr());
-    assertEquals(START_DATE_1, price.getStartDate());
-    assertEquals(END_DATE_1, price.getEndDate());
+    PriceQuery priceQuery = new PriceQuery(requestTime, productId, brandId);
+    Price priceResult = priceAdapter.findPrice(priceQuery);
+
+    assertNotNull(priceResult);
+    assertEquals(priceExpected.getId(), priceResult.getId());
+    assertEquals(priceExpected.getBrandId(), priceResult.getBrandId());
+    assertEquals(priceExpected.getProductId(), priceResult.getProductId());
+    assertEquals(priceExpected.getPriceList(), priceResult.getPriceList());
+    assertEquals(priceExpected.getCurr(), priceResult.getCurr());
+    assertEquals(priceExpected.getFinalPrice(), priceResult.getFinalPrice());
+    assertEquals(priceExpected.getStartDate(), priceResult.getStartDate());
+    assertEquals(priceExpected.getEndDate(), priceResult.getEndDate());
   }
 
   @Test
+  @DisplayName("Given existing price, when findPrice is called, then returns expected price (Test 2)")
   void givenExistingPrice_whenFindPrice_thenReturnsPriceTest2() {
     LocalDateTime requestTime = REQUEST_TIME_2;
     Integer productId = PRODUCT_ID_2;
     Integer brandId = BRAND_ID_2;
 
-    Optional<PriceEntity> priceEntityOptional = priceRepository.findPVP(brandId, productId, requestTime);
+    Mockito.when(priceRepository.findPVP(brandId, productId, requestTime))
+        .thenReturn(Optional.of(PriceEntity.builder()
+            .brandId(BRAND_ID_2)
+            .productId(PRODUCT_ID_2)
+            .priceList(PRICE_LIST_2)
+            .curr(CURRENCY_VALUE_2)
+            .finalPrice(FINAL_PRICE_2)
+            .startDate(START_DATE_2)
+            .endDate(END_DATE_2)
+            .build()));
 
-    assertNotNull(priceEntityOptional);
-    assertFalse(priceEntityOptional.isEmpty());
-    PriceEntity price = priceEntityOptional.get();
+    Price priceExpected = Price.builder()
+        .brandId(BRAND_ID_2)
+        .productId(PRODUCT_ID_2)
+        .priceList(PRICE_LIST_2)
+        .curr(CURRENCY_VALUE_2)
+        .finalPrice(FINAL_PRICE_2)
+        .startDate(START_DATE_2)
+        .endDate(END_DATE_2)
+        .build();
 
-    assertEquals(FINAL_PRICE_2, price.getFinalPrice());
-    assertEquals(BRAND_ID_2, price.getBrandId());
-    assertEquals(PRODUCT_ID_2, price.getProductId());
-    assertEquals(PRICE_LIST_2, price.getPriceList());
-    assertEquals(CURRENCY_VALUE_2, price.getCurr());
-    assertEquals(START_DATE_2, price.getStartDate());
-    assertEquals(END_DATE_2, price.getEndDate());
+    PriceQuery priceQuery = new PriceQuery(requestTime, productId, brandId);
+    Price priceResult = priceAdapter.findPrice(priceQuery);
+
+    assertNotNull(priceResult);
+    assertEquals(priceExpected.getId(), priceResult.getId());
+    assertEquals(priceExpected.getBrandId(), priceResult.getBrandId());
+    assertEquals(priceExpected.getProductId(), priceResult.getProductId());
+    assertEquals(priceExpected.getPriceList(), priceResult.getPriceList());
+    assertEquals(priceExpected.getCurr(), priceResult.getCurr());
+    assertEquals(priceExpected.getFinalPrice(), priceResult.getFinalPrice());
+    assertEquals(priceExpected.getStartDate(), priceResult.getStartDate());
+    assertEquals(priceExpected.getEndDate(), priceResult.getEndDate());
   }
 
   @Test
+  @DisplayName("Given existing price, when findPrice is called, then returns expected price (Test 3)")
   void givenExistingPrice_whenFindPrice_thenReturnsPriceTest3() {
     LocalDateTime requestTime = REQUEST_TIME_3;
     Integer productId = PRODUCT_ID_3;
     Integer brandId = BRAND_ID_3;
 
-    Optional<PriceEntity> priceEntityOptional = priceRepository.findPVP(brandId, productId, requestTime);
+    Mockito.when(priceRepository.findPVP(brandId, productId, requestTime))
+        .thenReturn(Optional.of(PriceEntity.builder()
+            .brandId(BRAND_ID_3)
+            .productId(PRODUCT_ID_3)
+            .priceList(PRICE_LIST_3)
+            .curr(CURRENCY_VALUE_3)
+            .finalPrice(FINAL_PRICE_3)
+            .startDate(START_DATE_3)
+            .endDate(END_DATE_3)
+            .build()));
 
-    assertNotNull(priceEntityOptional);
-    assertFalse(priceEntityOptional.isEmpty());
-    PriceEntity price = priceEntityOptional.get();
+    PriceQuery priceQuery = new PriceQuery(requestTime, productId, brandId);
+    Price priceResult = priceAdapter.findPrice(priceQuery);
 
-    assertEquals(FINAL_PRICE_3, price.getFinalPrice());
-    assertEquals(BRAND_ID_3, price.getBrandId());
-    assertEquals(PRODUCT_ID_3, price.getProductId());
-    assertEquals(PRICE_LIST_3, price.getPriceList());
-    assertEquals(CURRENCY_VALUE_3, price.getCurr());
-    assertEquals(START_DATE_3, price.getStartDate());
-    assertEquals(END_DATE_3, price.getEndDate());
+    assertNotNull(priceResult);
+    assertEquals(BRAND_ID_3, priceResult.getBrandId());
+    assertEquals(PRODUCT_ID_3, priceResult.getProductId());
+    assertEquals(PRICE_LIST_3, priceResult.getPriceList());
+    assertEquals(CURRENCY_VALUE_3, priceResult.getCurr());
+    assertEquals(FINAL_PRICE_3, priceResult.getFinalPrice());
+    assertEquals(START_DATE_3, priceResult.getStartDate());
+    assertEquals(END_DATE_3, priceResult.getEndDate());
   }
 
   @Test
+  @DisplayName("Given existing price, when findPrice is called, then returns expected price (Test 4)")
   void givenExistingPrice_whenFindPrice_thenReturnsPriceTest4() {
     LocalDateTime requestTime = REQUEST_TIME_4;
     Integer productId = PRODUCT_ID_4;
     Integer brandId = BRAND_ID_4;
 
-    Optional<PriceEntity> priceEntityOptional = priceRepository.findPVP(brandId, productId, requestTime);
+    Mockito.when(priceRepository.findPVP(brandId, productId, requestTime))
+        .thenReturn(Optional.of(PriceEntity.builder()
+            .brandId(BRAND_ID_4)
+            .productId(PRODUCT_ID_4)
+            .priceList(PRICE_LIST_4)
+            .curr(CURRENCY_VALUE_4)
+            .finalPrice(FINAL_PRICE_4)
+            .startDate(START_DATE_4)
+            .endDate(END_DATE_4)
+            .build()));
 
-    assertNotNull(priceEntityOptional);
-    assertFalse(priceEntityOptional.isEmpty());
-    PriceEntity price = priceEntityOptional.get();
+    Price priceExpected = Price.builder()
+        .brandId(BRAND_ID_4)
+        .productId(PRODUCT_ID_4)
+        .priceList(PRICE_LIST_4)
+        .curr(CURRENCY_VALUE_4)
+        .finalPrice(FINAL_PRICE_4)
+        .startDate(START_DATE_4)
+        .endDate(END_DATE_4)
+        .build();
 
-    assertEquals(FINAL_PRICE_4, price.getFinalPrice());
-    assertEquals(BRAND_ID_4, price.getBrandId());
-    assertEquals(PRODUCT_ID_4, price.getProductId());
-    assertEquals(PRICE_LIST_4, price.getPriceList());
-    assertEquals(CURRENCY_VALUE_4, price.getCurr());
-    assertEquals(START_DATE_4, price.getStartDate());
-    assertEquals(END_DATE_4, price.getEndDate());
+    PriceQuery priceQuery = new PriceQuery(requestTime, productId, brandId);
+    Price priceResult = priceAdapter.findPrice(priceQuery);
+
+    assertNotNull(priceResult);
+    assertEquals(priceExpected.getId(), priceResult.getId());
+    assertEquals(priceExpected.getBrandId(), priceResult.getBrandId());
+    assertEquals(priceExpected.getProductId(), priceResult.getProductId());
+    assertEquals(priceExpected.getPriceList(), priceResult.getPriceList());
+    assertEquals(priceExpected.getCurr(), priceResult.getCurr());
+    assertEquals(priceExpected.getFinalPrice(), priceResult.getFinalPrice());
+    assertEquals(priceExpected.getStartDate(), priceResult.getStartDate());
+    assertEquals(priceExpected.getEndDate(), priceResult.getEndDate());
   }
 
   @Test
+  @DisplayName("Given existing price, when findPrice is called, then returns expected price (Test 5)")
   void givenExistingPrice_whenFindPrice_thenReturnsPriceTest5() {
     LocalDateTime requestTime = REQUEST_TIME_5;
     Integer productId = PRODUCT_ID_5;
     Integer brandId = BRAND_ID_5;
 
-    Optional<PriceEntity> priceEntityOptional = priceRepository.findPVP(brandId, productId, requestTime);
+    Mockito.when(priceRepository.findPVP(brandId, productId, requestTime))
+        .thenReturn(Optional.of(PriceEntity.builder()
+            .brandId(BRAND_ID_5)
+            .productId(PRODUCT_ID_5)
+            .priceList(PRICE_LIST_5)
+            .curr(CURRENCY_VALUE_5)
+            .finalPrice(FINAL_PRICE_5)
+            .startDate(START_DATE_5)
+            .endDate(END_DATE_5)
+            .build()));
 
-    assertNotNull(priceEntityOptional);
-    assertFalse(priceEntityOptional.isEmpty());
-    PriceEntity price = priceEntityOptional.get();
+    Price priceExpected = Price.builder()
+        .brandId(BRAND_ID_5)
+        .productId(PRODUCT_ID_5)
+        .priceList(PRICE_LIST_5)
+        .curr(CURRENCY_VALUE_5)
+        .finalPrice(FINAL_PRICE_5)
+        .startDate(START_DATE_5)
+        .endDate(END_DATE_5)
+        .build();
 
-    assertEquals(FINAL_PRICE_5, price.getFinalPrice());
-    assertEquals(BRAND_ID_5, price.getBrandId());
-    assertEquals(PRODUCT_ID_5, price.getProductId());
-    assertEquals(PRICE_LIST_5, price.getPriceList());
-    assertEquals(CURRENCY_VALUE_5, price.getCurr());
-    assertEquals(START_DATE_5, price.getStartDate());
-    assertEquals(END_DATE_5, price.getEndDate());
+    PriceQuery priceQuery = new PriceQuery(requestTime, productId, brandId);
+    Price priceResult = priceAdapter.findPrice(priceQuery);
+
+    assertNotNull(priceResult);
+    assertEquals(priceExpected.getId(), priceResult.getId());
+    assertEquals(priceExpected.getBrandId(), priceResult.getBrandId());
+    assertEquals(priceExpected.getProductId(), priceResult.getProductId());
+    assertEquals(priceExpected.getPriceList(), priceResult.getPriceList());
+    assertEquals(priceExpected.getCurr(), priceResult.getCurr());
+    assertEquals(priceExpected.getFinalPrice(), priceResult.getFinalPrice());
+    assertEquals(priceExpected.getStartDate(), priceResult.getStartDate());
+    assertEquals(priceExpected.getEndDate(), priceResult.getEndDate());
   }
 
   @Test
+  @DisplayName("Given non-existing price, when findPrice is called, then returns empty result")
   void givenNonExistingPrice_whenFindPrice_thenReturnsEmpty() {
     LocalDateTime requestTime = LocalDateTime.of(2020, 6, 14, 10, 0, 0);
     Integer productId = 1234;
     Integer brandId = 1234;
 
-    Optional<PriceEntity> priceEntityOptional = priceRepository.findPVP(brandId, productId, requestTime);
+    Mockito.when(priceRepository.findPVP(brandId, productId, requestTime))
+        .thenReturn(Optional.empty());
 
-    assertNotNull(priceEntityOptional);
-    assertTrue(priceEntityOptional.isEmpty());
+    PriceQuery priceQuery = new PriceQuery(requestTime, productId, brandId);
+    Price priceResult = priceAdapter.findPrice(priceQuery);
+
+    assertNull(priceResult);
   }
 
   @Test
+  @DisplayName("Given non-existing price, when findPrice is called, then returns empty result (Test 2)")
   void givenNonExistingPrice_whenFindPrice_thenReturnsEmpty2() {
     LocalDateTime requestTime = LocalDateTime.of(2020, 6, 14, 10, 0, 0);
     Integer productId = 1234;
     Integer brandId = 1234;
 
-    Optional<PriceEntity> priceEntityOptional = priceRepository.findPVP(brandId, productId, requestTime);
+    Mockito.when(priceRepository.findPVP(brandId, productId, requestTime))
+        .thenReturn(Optional.empty());
 
-    assertNotNull(priceEntityOptional);
-    assertTrue(priceEntityOptional.isEmpty());
+    PriceQuery priceQuery = new PriceQuery(requestTime, productId, brandId);
+    Price priceResult = priceAdapter.findPrice(priceQuery);
+
+    assertNull(priceResult);
   }
 
   @Test
+  @DisplayName("When findAllPrices is called, then returns all prices")
   void whenFindAllPrices_thenReturnsAllPrices() {
-    List<PriceEntity> prices = priceRepository.findAll();
+    Mockito.when(priceRepository.findAll())
+        .thenReturn(List.of(
+            PriceEntity.builder()
+                .brandId(BRAND_ID_1)
+                .productId(PRODUCT_ID_1)
+                .priceList(PRICE_LIST_1)
+                .curr(CURRENCY_VALUE_1)
+                .finalPrice(FINAL_PRICE_1)
+                .startDate(START_DATE_1)
+                .endDate(END_DATE_1)
+                .build(),
+            PriceEntity.builder()
+                .brandId(BRAND_ID_2)
+                .productId(PRODUCT_ID_2)
+                .priceList(PRICE_LIST_2)
+                .curr(CURRENCY_VALUE_2)
+                .finalPrice(FINAL_PRICE_2)
+                .startDate(START_DATE_2)
+                .endDate(END_DATE_2)
+                .build(),
+            PriceEntity.builder()
+                .brandId(BRAND_ID_3)
+                .productId(PRODUCT_ID_3)
+                .priceList(PRICE_LIST_3)
+                .curr(CURRENCY_VALUE_3)
+                .finalPrice(FINAL_PRICE_3)
+                .startDate(START_DATE_3)
+                .endDate(END_DATE_3)
+                .build(),
+            PriceEntity.builder()
+                .brandId(BRAND_ID_4)
+                .productId(PRODUCT_ID_4)
+                .priceList(PRICE_LIST_4)
+                .curr(CURRENCY_VALUE_4)
+                .finalPrice(FINAL_PRICE_4)
+                .startDate(START_DATE_4)
+                .endDate(END_DATE_4)
+                .build()));
+    List<Price> allPrices = priceAdapter.findAll();
 
-    assertNotNull(prices);
-    assertEquals(4, prices.size());
+    assertNotNull(allPrices);
+    assertEquals(4, allPrices.size());
   }
 }
